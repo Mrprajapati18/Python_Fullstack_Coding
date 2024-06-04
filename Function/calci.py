22f17c03549c918e944b83eff7eb7f1037a45ec7
Durgesh Prajapati <durgeshkumar778080@gmail.com>
2024-06-04 23:39:47 +0530

    commit

---
+++ Function/calci.py
@@ -0,0 +1,18 @@
+a,b = 20,5;
+
+sum = a+b;
+print(sum);
+print("sum = ",sum);
+
+diff = a-b;
+print(diff);
+print("diff = ",diff);
+
+
+Mul = a*b;
+print(Mul);
+print("Mul = ",Mul);
+
+div = a/b;
+print(div);
+print("div = ",div);
