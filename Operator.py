8343d4a71ffefcb3209af2f1de5fc89ce083d15f
Durgesh Prajapati <durgeshkumar778080@gmail.com>
2024-06-04 23:12:07 +0530

    Commit

---
+++ Operator.py
@@ -0,0 +1,47 @@
+a,b,c =10, 5,15;
+print(a);
+print(b);
+
+# Arithmetic operator
+print(a+b);
+print(a-b);
+print(a*b);
+print(a/b);
+print(a%b);
+print(a ** b);
+
+# comparison Operator
+
+print(a==b);
+print(a>c);
+
+print(a<c);
+print(a>b);
+
+print(a>=c);
+print(a<=b);
+
+print(a !=b);
+
+
+# Logical Operator (And)
+print(10>5 and 15>5);
+print(10>5 and 15>50);
+print(10>55 and 15>5);
+print(10>50 and 15>50);
+
+
+# Logical Operator (Or)
+print(10>5 or 15>5);    #true or true
+
+print(10>5 or 15>50);   # true or false
+
+print(10>55 or 15>5);   # false or true
+
+print(10>50 or 15>50);  # false or false
+
+# Logical Operator(Not)
+
+print(not(10>5));   #not true
+
+print(not(20<5));   # not false
---
+++ datatype.py
@@ -0,0 +1,42 @@
+
+#int dta types
+a = 10;
+print(a);
+print(type(a));
+
+# float datatypes
+b = 10.999;
+print(b);
+print(type(b));
+
+# complex number  
+c = 10 +5j;
+print(c);
+print(type(c));
+
+# Character data Types
+d = 'Deep';
+print(d);
+print(type(d));
+
+e = "Durgesh";
+print(e);
+print(type(e));
+
+f = '''Deepak''';
+print(f);
+print(type(f));
+
+g = " " " Dindyal" " ";
+print(g);
+print(type(g));
+
+# Bloonean Datatypes
+
+h = True;
+print(h);
+print(type(h));
+
+I = False;
+print(I);
+print(type(I));
