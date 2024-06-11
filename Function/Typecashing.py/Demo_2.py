c92760d497ce25cf03162e8b8f45535ce48cdadd
Durgesh Prajapati <durgeshkumar778080@gmail.com>
2024-06-12 00:09:13 +0530

    Commit

---
+++ Function/Typecashing.py/Demo_2.py
@@ -0,0 +1,10 @@
+# Int typecasting
+data = int (input("Enter a integer number"));
+print("user data :",data);
+print(type(data));
+
+# Float typecasting
+data_1 = float(input("Enter the float Number"));
+print("user Data_1:",data_1);
+print(type(data_1));
+
